003f2af85645c39e90703665d105f5424a93a2b9
Krutika Kulkarni <krutika.kulkarni@mavs.uta.edu>
2017-05-20 13:59:24 -0500

    Add files via upload

---
+++ crmsFront.php
@@ -0,0 +1,70 @@
+<html>
+<head>
+ <title>CRIME RECORD MANAGEMENT</title>
+ <h1 align ="center">Crime Record Management</h1>
+ <!-- Bootstrap core CSS -->
+ <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+
+ <!-- Custom styles for this template -->
+ <link href="starter-template.css" rel="stylesheet">
+
+</head>
+
+<style>
+table, th, td {
+  border: 1px solid black;
+}
+body.one {
+    background-color: white;
+    padding-top: 100px;
+    padding-right: 30px;
+    padding-bottom: 50px;
+    padding-left: 80px;
+  }
+
+  .wrapper {
+      text-align: center;
+  }
+
+  .button {
+      position: absolute;
+      top: 20%;
+      right: 45%;
+  }
+  .paragraph{
+    position: absolute;
+    top: 30%;
+    right: 45%;
+    left:10%;
+  }
+  .image{
+    position: absolute;
+    top:30%;
+    right:5%
+  }
+</style>
+<body>
+<div class="wrapper">
+  <form action="crimeRecord.php" method="POST"><br>
+    <button class="button">Click to Continue</button><br><br>
+
+</div>
+<!--  <input type="submit" value="Click To Continue" name="Click To Continue"/><br><br><br>-->
+</form>
+<div>
+<div class="paragraph" width="70%" align="left">
+<p align= "justify" ><b>ABSTRACT</b><br><br>
+<font size= "3">Until today a citizen had to go to Police station in person to report a crime and anonymous events like thefts, murder and other events. 
+The process of resolving the case involved a lot of manual work. As we know manual work is error prone. This application we are building is an online crime
+reporting system to improve the communication between police and public which will help improve time utilization in solving crime and much time wasted to communicate with police.
+We will be able to increase problem solving efficiency in time period using this application. The proposed system applies to all Police Institutions across 
+the country and specifically looks in to Crime record management.There is a prominent requirement of highly responsive Information management system as a backbone to manage and retrieve data at a high speed. 
+</font>
+</p>
+</div>
+<div class="image" align="right">
+  <img src="crime_record.jpg" width="500"  />
+</div>
+</div>
+</body>
+</html>
